a73c6639c59814d19af1ee10b7c67c5267587447
Marti Bolivar <mbolivar@leaflabs.com>
2014-01-15 17:36:17 -0500

    conf.py: allow doxygen XML output in repository root
    It's a pain to have to re-make the XML output for a given release, and
    there's a chance it'll change between different versions of
    Doxygen. So look for Doxygen XML output in doxygen/xml/ relative to
    the repository root.
    
    This allows maintenance branches to check in the XML for their version
    of libmaple.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,7 @@
 # serve to show the default.
 
 import sys, os
+import os.path
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -267,7 +268,17 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 
 # -- Options for breathe integration ------------------------------------------
 
-breathe_projects = {'libmaple' : os.path.join(os.environ['LIB_MAPLE_HOME'],
-                                              'doxygen', 'xml')}
+# We allow people to check in Doxygen XML into maintenance branches.
+doxygen_xml_maybe = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                 'doxygen', 'xml')
+if os.path.isdir(doxygen_xml_maybe):
+    # There's a doxygen/xml/ directory in this repository root, use that.
+    doxygen_xml_dir = doxygen_xml_maybe
+else:
+    # Nothing there, assume it's in LIB_MAPLE_HOME.
+    doxygen_xml_dir = os.path.join(os.environ['LIB_MAPLE_HOME'],
+                                   'doxygen', 'xml')
+
+breathe_projects = {'libmaple' : doxygen_xml_dir}
 
 breathe_default_project = 'libmaple'
